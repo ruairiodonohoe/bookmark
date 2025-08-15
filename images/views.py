3a1d205792e6bc741a49ad4b29302eef0c4d8434
Ruairi O'Donohoe <58832267+ruairiodonohoe@users.noreply.github.com>
2025-08-15 09:11:54 +0100

    scrolling images

--- images/views.py
+++ images/views.py
@@ -1,6 +1,7 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.http import JsonResponse
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.decorators.http import require_POST
 from .forms import ImageCreateForm
@@ -36,7 +37,7 @@ def image_detail(request, id, slug):
 
 @login_required
 @require_POST
-def image_lit(request):
+def image_like(request):
     image_id = request.POST.get("id")
     action = request.POST.get("action")
     if image_id and action:
@@ -50,3 +51,32 @@ def image_lit(request):
         except Image.DoesNotExist:
             pass
     return JsonResponse({"status": "error"})
+
+
+@login_required
+def image_list(request):
+    images = Image.objects.all()
+    paginator = Paginator(images, 8)
+    page = request.GET.get("page")
+    images_only = request.GET.get("images_only")
+    try:
+        images = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer deliver the first page
+        images = paginator.page(1)
+    except EmptyPage:
+        if images_only:
+            # If AJAX request and page out of range
+            # return an empty page
+            return HttpResponse("")
+        # If page out of range return last page of results
+        images = paginator.page(paginator.num_pages)
+    if images_only:
+        return render(
+            request,
+            "images/image/list_images.html",
+            {"section": "images", "images": images},
+        )
+    return render(
+        request, "images/image/list.html", {"section": "images", "images": images}
+    )
